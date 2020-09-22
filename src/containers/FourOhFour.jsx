be5878bf7922ab0c36ceab106a19749fe6bb2d8e
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-09-22 18:53:44 +0530

    :sparkles: add back button to 404 page

--- src/containers/FourOhFour.jsx
+++ src/containers/FourOhFour.jsx
@@ -27,13 +27,40 @@ const Child = styled.div`
   text-align: center;
 `;
 
-const FourOhFour = () => (
-  <Container>
-    <Child>
-      <h1>Four Oh Four</h1>
-      <h2>Seems like, you are lost just like me</h2>
-    </Child>
-  </Container>
-);
+const Back = styled.div`
+  margin-top: 1rem;
+  line-height: 25px;
+  color: #2d9cdb;
+  cursor: pointer;
+
+  .back-icon {
+    font-size: 15px;
+    margin-right: 2.5px;
+  }
+
+  .back-text {
+    text-decoration: none;
+    color: #2d9cdb;
+    font-size: 15px;
+  }
+`;
+
+const FourOhFour = (props) => {
+  const letsGoBack = () => props.history.goBack();
+
+  return (
+    <Container>
+      <Child>
+        <h1>Four Oh Four</h1>
+        <h2>Seems like, you are lost just like me</h2>
+        <Back>
+          <div className="back-text" onClick={letsGoBack}>
+            Back
+          </div>
+        </Back>
+      </Child>
+    </Container>
+  );
+};
 
 export default FourOhFour;
